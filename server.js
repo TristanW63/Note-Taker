0da315884dcd7f93cce5b0a3ef55e634270b2998
Tristan Barron <tristan_wilson_63@yahoo.com>
2022-10-18 23:10:10 -0500

    trying

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const apiRoute = require('./routes/apiRoutes');
-
-const uuid = require('./helpers/uuid');
+// const htmlRoute = require('./routes/htmlRoute');
+// const apiRoute = require('./routes/apiRoutes');
 
 const PORT = 3001;
 
@@ -12,66 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-app.use('/api', apiRoute);
+// app.use('/api', apiRoute);
+// create route for html route
+// app.use('/', htmlRoute);
 
+app.use(require('./routes/apiRoutes'));
 
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('/api/db', (req, res) => {
-   res.status(200).json(`${req.method} request received`);
-
-   console.info(`${req.method} request received`)
-});
-
-app.post('/api/db', (req, res) => {
-    console.info(`${req.method} request received to add note`);
-
-    const { title, text } = req.body;
-
-    if(title && text) {
-        const newNote = {
-            title,
-            text,
-            note_id: uuid(),
-        };
-
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if(err) {
-                console.error(err)
-            } else {
-                const parsedNotes = JSON.parse(data);
-
-                parsedNotes.push(newNote);
-
-                fs.writeFile(
-                './db/db.json',
-                JSON.stringify(parsedNotes, null, 4),
-                (writeErr) =>
-                writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated notes!')
-                );
-            }
-        });
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response)
-    } else {
-        res.status(500).json('Error in posting note');
-    }
-});
-
+   
 app.listen(PORT, () =>
 console.log(`App listening at  http://localhost:${PORT} 🚀`)
 );
